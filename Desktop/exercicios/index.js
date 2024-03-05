32206e81af51690ef2dce39005a2996469f3daca
ANA CAROLINA LOCKS GOTTERT <03440521095@senacrs.edu.br>
2024-03-05 11:55:34 -0300

    exercicios

---
+++ Desktop/exercicios/index.js
@@ -0,0 +1,32 @@
+// Comando para exibir mensagem 
+console.log("Hello World")
+
+//exercicios
+
+const num1 = 100;
+const num2 = 10;
+
+//soma
+
+let soma = num1 + num2;
+
+console.log("O resultado é " + soma + "CERTOO");
+
+// subtração
+
+let subtração = num1 - num2;
+
+console.log( "O resultado é " + subtração + " CERTOO");
+
+//multiplicação
+
+
+let multiplicação = num1 * num2;
+
+console.log( "O resultado é " + multiplicação + " CERTOO");
+
+//divisão
+
+let divisão = num1 / num2;
+
+console.log( "O resultado é " + divisão + " CERTOO");
