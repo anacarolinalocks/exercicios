c78581fbbdcb2f5e018b27e7805a91636de2e4c3
ANA CAROLINA LOCKS GOTTERT <03440521095@senacrs.edu.br>
2024-03-08 11:42:31 -0300

    votação

---
+++ Desktop/exercicios/voto.js
@@ -0,0 +1,9 @@
+const aninha = 16;
+
+if (aninha >=18) {
+    console.log("Voto obrigatorio")
+} else if (aninha >=16) {
+    console.log("Voto facultativo")
+} else if (aninha <16) {
+    console.log("Não pode votar")
+}
